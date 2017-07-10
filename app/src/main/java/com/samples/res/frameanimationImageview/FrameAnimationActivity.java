54b3f8d20d86d25d49f2962c026d161869f91320
java2019 <java_2019@mail.ru>
2017-07-10 11:53:28 +0300

    Initial commit

---
+++ app/src/main/java/com/samples/res/frameanimationImageview/FrameAnimationActivity.java
@@ -0,0 +1,13 @@
+package com.samples.res.frameanimationImageview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FrameAnimationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_frame_animation);
+    }
+}
